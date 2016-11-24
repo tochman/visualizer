2a8542cb3298276117873870ed6a07e775c75840
tochman <thomas@craftacademy.se>
2016-11-24 22:15:40 +0100

    adds Dotenv and 'google-api-client' gems adds basic scenario to 'access_ga_profile.feature'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,44 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def redirect
+    client = Signet::OAuth2::Client.new({
+                                            client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
+                                            client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+                                            authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+                                            scope: Google::Apis::AnalyticsV3::AUTH_ANALYTICS_READONLY,
+                                            redirect_uri: url_for(action: :callback)
+                                        })
+    binding.pry
+    redirect_to client.authorization_uri.to_s
+  end
+
+  def callback
+    client = Signet::OAuth2::Client.new({
+                                            client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
+                                            client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+                                            token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+                                            redirect_uri: url_for(action: :callback),
+                                            code: params[:code]
+                                        })
+
+    response = client.fetch_access_token!
+
+    session[:access_token] = response['access_token']
+    binding.pry
+
+    redirect_to url_for(action: :analytics)
+  end
+
+  def analytics
+    client = Signet::OAuth2::Client.new(access_token: session[:access_token])
+
+    service = Google::Apis::AnalyticsV3::AnalyticsService.new
+
+    service.authorization = client
+    binding.pry
+
+    @account_summaries = service.list_account_summaries
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'setup', controller: :pages, action: :redirect
+  get 'callback', controller: :pages, action: :callback
   root controller: :pages, action: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
