e089deae7bde7195e66bf15f553ada440836c534
tochman <thomas@craftacademy.se>
2016-11-26 20:10:38 +0100

    adds info about visitors to report

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'setup', controller: :pages, action: :redirect
   get 'callback', controller: :pages, action: :callback
   get 'analytics', controller: :pages, action: :analytics
-  get 'get_data', controller: :pages, action: :get_data, as: :get_data
+  post 'get_data', controller: :pages, action: :get_data, as: :get_data
 
   root controller: :pages, action: :index
 
